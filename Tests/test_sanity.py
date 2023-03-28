aec086d744065de526f45e03c99dc9579bbd6985
Ilay <IlayGo97@gmail.com>
2023-03-28 17:01:45 +0300

    pre commit hooks and .toml file

---
+++ Tests/test_sanity.py
@@ -0,0 +1,12 @@
+import unittest
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True, "Fail message")  # add assertion here
+
+    def shortDescription(self) -> str | None:
+        return "Unit test description to show in case of fail"
+
+
+if __name__ == '__main__':
+    unittest.main()
