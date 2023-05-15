f2b362784d68f197fbb44eb3428ea95fbb60cd0a
Oded Gal <odedgal67@gmail.com>
2023-05-15 14:51:09 +0300

    finished apartments implementation

--- Apartment.py
+++ Apartment.py
@@ -23,3 +23,56 @@ class Apartment:
         if stage_id not in self.stages.keys():
             raise StageDoesntExistException()
         return self.stages[stage_id]
+
+    def add_stage(self, stage_name: str):
+        if self.contains_stage_name(stage_name):
+            raise DuplicateStageNameException(stage_name)
+        new_stage: Stage = Stage(stage_name)
+        self.stages[new_stage.id] = new_stage
+        return new_stage
+
+    def set_stage_status(self, stage_id: UUID, new_status):
+        stage: Stage = self.get_stage(stage_id)
+        return stage.set_status(new_status)
+
+    def edit_stage_name(self, stage_id, new_stage_name):
+        if self.contains_stage_name(new_stage_name):
+            raise DuplicateStageNameException(new_stage_name)
+        stage: Stage = self.get_stage(stage_id)
+        return stage.edit_name(new_stage_name)
+
+    def set_mission_status(self, stage_id, mission_id, new_status, username):
+        stage: Stage = self.get_stage(stage_id)
+        return stage.set_mission_status(mission_id, new_status, username)
+
+    def add_mission(self, stage_id, mission_name):
+        stage: Stage = self.get_stage(stage_id)
+        return stage.add_mission(mission_name)
+
+    def get_all_missions(self, stage_id):
+        stage: Stage = self.get_stage(stage_id)
+        return stage.get_all_missions()
+
+    def get_all_stages(self):
+        return list(self.stages.values())
+
+    def edit_comment_in_mission(self, stage_id, mission_id, comment):
+        stage: Stage = self.get_stage(stage_id)
+        return stage.edit_comment_in_mission(mission_id, comment)
+
+    def remove_stage(self, stage_id):
+        if not self.contains_stage_id(stage_id):
+            raise StageDoesntExistException()
+        return self.stages.pop(stage_id)
+
+    def remove_mission(self, stage_id, mission_id):
+        stage: Stage = self.get_stage(stage_id)
+        return stage.remove_mission(mission_id)
+
+    def set_green_building(self, stage_id, mission_id, is_green_building):
+        stage: Stage = self.get_stage(stage_id)
+        return stage.set_green_building(mission_id, is_green_building)
+
+    def is_mission_invalid(self, stage_id, mission_id):
+        stage: Stage = self.get_stage(stage_id)
+        return stage.is_mission_invalid(mission_id)
